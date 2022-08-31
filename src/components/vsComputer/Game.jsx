a623131696cbee19c16349ace5e32f50c0d6ded1
ayberkuman <ayberkuman1@gmail.com>
2022-08-31 18:05:10 +0300

    finished

--- src/components/vsComputer/Game.jsx
+++ src/components/vsComputer/Game.jsx
@@ -39,17 +39,14 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext,
     };
 
-    // Return a Promise that resolves when setState completes.
     return new Promise((resolve, reject) => {
       this.setState(nextState, resolve);
     });
   }
 
   async handleClick(i) {
-    // Apply player move to square i
     await this.makeMove(i);
 
-    // Apply AI move
     const squares = this.state.history[this.state.stepNumber].squares.slice();
     const bestSquare = findBestSquare(squares, this.state.xIsNext ? "X" : "O");
     if (bestSquare !== -1) {
